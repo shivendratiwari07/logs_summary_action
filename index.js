13b6bda3dadda2384d74aea0034096606abedb1d
Your Name <you@example.com>
2024-08-22 15:15:28 +0530

    js action

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ async function run() {
     const repoOwner = core.getInput('repo_owner');
     const repoName = core.getInput('repo_name');
     const githubToken = core.getInput('github_token');
-    const customServiceCookie = core.getInput('custom_service_cookie'); 
+    const customServiceCookie = process.env.CUSTOM_SERVICE_COOKIE;
+    //const customServiceCookie = core.getInput('custom_service_cookie'); 
 
     console.log(`run_id: ${runId}`);
     console.log(`repo_owner: ${repoOwner}`);
